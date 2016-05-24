6b097e768165fb910d633fc3ee339dd9e1e59c36
Daniel Ohashi <danielohashi@Daniels-Mac-mini.local>
2016-05-23 23:09:30 -0400

    qsort without good duplicate support

--- qsort/main.cpp
+++ qsort/main.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
+#include <cstdlib>
+#include <utility>
 #include "darray.h"
 
+void qsort(int*left, int*right){
+	if(left>=right)return;
+	int* pivot = left + std::rand() % (1+right-left);
+	std::swap(*pivot,*right);
+	int *i=left;
+	int *j=right-1;
+	while(i<=j){
+		std::cout << *i << " " << *j <<std::endl;
+		if(*i <= *right){
+			i++;
+		}else if(*j > *right){
+			j--;
+		}else{
+			std::swap(*j,*i);
+			j--;
+			i++;
+		}
+	}
+	if(i==right){
+		qsort(left,i-1);
+		return;
+	}
+	std::swap(*right,*i);
+	qsort(left,i-1);
+	qsort(i+1,right);
+}
+
 void qsort(DArray<int>& arr){
-	
+	if(arr.size()>0)
+	qsort(&arr[0],&arr[arr.size()-1]);
 }
 
 int main(){
